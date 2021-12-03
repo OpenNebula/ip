86ee98683ca9de7e383c02edbab8dff5b2c77069
Sergio Betanzos <sbetanzos@opennebula.io>
2021-12-03 18:06:27 +0100

    Add config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,139 @@
+/* ------------------------------------------------------------------------- *
+ * Copyright 2002-2021, OpenNebula Project, OpenNebula Systems               *
+ *                                                                           *
+ * Licensed under the Apache License, Version 2.0 (the 'License'); you may   *
+ * not use this file except in compliance with the License. You may obtain   *
+ * a copy of the License at                                                  *
+ *                                                                           *
+ * http://www.apache.org/licenses/LICENSE-2.0                                *
+ *                                                                           *
+ * Unless required by applicable law or agreed to in writing, software       *
+ * distributed under the License is distributed on an 'AS IS' BASIS,         *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ * See the License for the specific language governing permissions and       *
+ * limitations under the License.                                            *
+ * ------------------------------------------------------------------------- */
+
+const RULES = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error',
+}
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:jsdoc/recommended',
+    'standard',
+    'prettier',
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+  },
+  plugins: ['jsx-a11y', 'jsdoc', 'header', 'prettier'],
+  rules: {
+    "default-case": RULES.OFF,
+    "prefer-const": [RULES.ERROR, {
+      "destructuring": "all"
+    }],
+    "no-restricted-syntax": [
+      RULES.ERROR,
+      "ForInStatement",
+      "LabeledStatement",
+      "WithStatement"
+    ],
+    "no-return-assign": [RULES.ERROR, "except-parens"],
+    "no-param-reassign": [RULES.ERROR, {
+      "props": false
+    }],
+    "no-shadow": [RULES.ERROR, {
+      "hoist": "all",
+      "allow": ["resolve", "reject", "done", "next", "err", RULES.ERROR, "children"]
+    }],
+    "arrow-body-style": [RULES.ERROR, "as-needed"],
+    "import/no-extraneous-dependencies": [RULES.ERROR, {
+      "devDependencies": ["*.js"]
+    }],
+    "padding-line-between-statements": [RULES.ERROR, {
+      "blankLine": "always",
+      "prev": "*",
+      "next": "return"
+    }],
+    "react/jsx-uses-react": RULES.OFF,
+    "react/react-in-jsx-scope": RULES.OFF,
+    "react/display-name": RULES.WARN,
+    "react/no-array-index-key": RULES.OFF,
+    "react/require-default-props": RULES.OFF,
+    "react/forbid-prop-types": RULES.OFF,
+    'jsx-a11y/alt-text': [RULES.WARN, { elements: ['img'], img: ['Image'] }],
+    'jsx-a11y/aria-props': RULES.WARN,
+    'jsx-a11y/aria-proptypes': RULES.WARN,
+    'jsx-a11y/aria-unsupported-elements': RULES.WARN,
+    'jsx-a11y/role-has-required-aria-props': RULES.WARN,
+    'jsx-a11y/role-supports-aria-props': RULES.WARN,
+    "prettier/prettier": [RULES.ERROR, {
+      "singleQuote": true,
+      "semi": false,
+      "trailingComma": "es5"
+    }],
+    'jsdoc/require-jsdoc': [
+      RULES.ERROR,
+      {
+        publicOnly: true,
+        require: {
+          ArrowFunctionExpression: true,
+          FunctionDeclaration: true,
+          FunctionExpression: true,
+          ClassDeclaration: true,
+          ClassExpression: true,
+          MethodDefinition: true,
+        },
+      },
+    ],
+    'jsdoc/check-indentation': RULES.WARN,
+    'jsdoc/check-line-alignment': RULES.WARN,
+    'jsdoc/check-syntax': RULES.WARN,
+    'jsdoc/match-description': RULES.WARN,
+    'jsdoc/require-asterisk-prefix': RULES.WARN,
+    'jsdoc/require-hyphen-before-param-description': RULES.WARN,
+    'header/header': [
+      RULES.ERROR,
+      'block',
+      [
+        ' ------------------------------------------------------------------------- *',
+        ' * Copyright 2002-2021, OpenNebula Project, OpenNebula Systems               *',
+        ' *                                                                           *',
+        " * Licensed under the Apache License, Version 2.0 (the 'License'); you may   *",
+        ' * not use this file except in compliance with the License. You may obtain   *',
+        ' * a copy of the License at                                                  *',
+        ' *                                                                           *',
+        ' * http://www.apache.org/licenses/LICENSE-2.0                                *',
+        ' *                                                                           *',
+        ' * Unless required by applicable law or agreed to in writing, software       *',
+        " * distributed under the License is distributed on an 'AS IS' BASIS,         *",
+        ' * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *',
+        ' * See the License for the specific language governing permissions and       *',
+        ' * limitations under the License.                                            *',
+        ' * ------------------------------------------------------------------------- ',
+      ],
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
---
+++ README.md
@@ -0,0 +1,64 @@
+# OpenNebula Eslint and Prettier Setup
+These are OpenNebula settings for ESLint and Prettier
+
+## What it does
+* Lints JavaScript based on the latest standards
+* Fixes issues and formatting errors with Prettier
+
+## Installing
+
+It's usually best to install this locally once per project, that way you can have project specific settings as well as sync those settings with others working on your project via git.
+
+1. If you don't already have a `package.json` file, create one with `npm init`.
+
+2. Then we need to install everything needed by the config:
+
+```
+npm install --save-dev eslint-config-opennebula
+```
+
+3. Create a `.eslintrc` file in the root of your project's directory (it should live where package.json does). Your `.eslintrc` file should look like this:
+
+```json
+{
+  "extends": [ "opennebula" ]
+}
+```
+
+
+Tip: You can alternatively put this object in your `package.json` under the property `"eslintConfig":`. This makes one less file in your project.
+
+4. You can add two scripts to your package.json to lint and/or fix:
+
+```json
+"scripts": {
+  "lint": "eslint .",
+  "lint:fix": "eslint . --fix"
+},
+```
+
+5. Now you can manually lint your code by running `npm run lint` and fix all fixable issues with `npm run lint:fix`.
+
+## With VS Code
+You probably want your editor to do this though.
+
+1. Install the [ESLint package](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint)
+2. Now we need to setup some VS Code settings via `Code/File` → `Preferences` → `Settings`. It's easier to enter these settings while editing the `settings.json` file, so click the Open (Open Settings) icon in the top right corner:
+  ```js
+  "editor.formatOnSave": true,
+  // turn it off for JS and JSX, we will do this via eslint
+  "[javascript]": {
+    "editor.formatOnSave": false
+  },
+  "[javascriptreact]": {
+    "editor.formatOnSave": false
+  },
+  // show eslint icon at bottom toolbar
+  "eslint.alwaysShowStatus": true,
+  // tell the ESLint plugin to run on save
+  "editor.codeActionsOnSave": {
+    "source.fixAll": true
+  }
+  ```
+
+Finally you'll usually need to restart VS code.
