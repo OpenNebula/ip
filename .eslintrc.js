bb7f9aa9584498eff9cb11bc5fc5d45367685546
Sergio Betanzos <sbetanzos@opennebula.io>
2021-12-03 19:05:20 +0100

    Fix headers

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 /* ------------------------------------------------------------------------- *
  * Copyright 2002-2021, OpenNebula Project, OpenNebula Systems               *
  *                                                                           *
- * Licensed under the Apache License, Version 2.0 (the 'License'); you may   *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may   *
  * not use this file except in compliance with the License. You may obtain   *
  * a copy of the License at                                                  *
  *                                                                           *
  * http://www.apache.org/licenses/LICENSE-2.0                                *
  *                                                                           *
  * Unless required by applicable law or agreed to in writing, software       *
- * distributed under the License is distributed on an 'AS IS' BASIS,         *
+ * distributed under the License is distributed on an "AS IS" BASIS,         *
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
  * See the License for the specific language governing permissions and       *
  * limitations under the License.                                            *
@@ -34,6 +34,7 @@ module.exports = {
   ],
   parser: '@babel/eslint-parser',
   parserOptions: {
+    requireConfigFile: false,
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
@@ -45,50 +46,76 @@ module.exports = {
   },
   plugins: ['jsx-a11y', 'jsdoc', 'header', 'prettier'],
   rules: {
-    "default-case": RULES.OFF,
-    "prefer-const": [RULES.ERROR, {
-      "destructuring": "all"
-    }],
-    "no-restricted-syntax": [
+    'default-case': RULES.OFF,
+    'prefer-const': [
       RULES.ERROR,
-      "ForInStatement",
-      "LabeledStatement",
-      "WithStatement"
+      {
+        destructuring: 'all',
+      },
     ],
-    "no-return-assign": [RULES.ERROR, "except-parens"],
-    "no-param-reassign": [RULES.ERROR, {
-      "props": false
-    }],
-    "no-shadow": [RULES.ERROR, {
-      "hoist": "all",
-      "allow": ["resolve", "reject", "done", "next", "err", RULES.ERROR, "children"]
-    }],
-    "arrow-body-style": [RULES.ERROR, "as-needed"],
-    "import/no-extraneous-dependencies": [RULES.ERROR, {
-      "devDependencies": ["*.js"]
-    }],
-    "padding-line-between-statements": [RULES.ERROR, {
-      "blankLine": "always",
-      "prev": "*",
-      "next": "return"
-    }],
-    "react/jsx-uses-react": RULES.OFF,
-    "react/react-in-jsx-scope": RULES.OFF,
-    "react/display-name": RULES.WARN,
-    "react/no-array-index-key": RULES.OFF,
-    "react/require-default-props": RULES.OFF,
-    "react/forbid-prop-types": RULES.OFF,
+    'no-restricted-syntax': [
+      RULES.ERROR,
+      'ForInStatement',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+    'no-return-assign': [RULES.ERROR, 'except-parens'],
+    'no-param-reassign': [
+      RULES.ERROR,
+      {
+        props: false,
+      },
+    ],
+    'no-shadow': [
+      RULES.ERROR,
+      {
+        hoist: 'all',
+        allow: [
+          'resolve',
+          'reject',
+          'done',
+          'next',
+          'err',
+          RULES.ERROR,
+          'children',
+        ],
+      },
+    ],
+    'arrow-body-style': [RULES.ERROR, 'as-needed'],
+    'import/no-extraneous-dependencies': [
+      RULES.ERROR,
+      {
+        devDependencies: ['*.js'],
+      },
+    ],
+    'padding-line-between-statements': [
+      RULES.ERROR,
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'return',
+      },
+    ],
+    'react/jsx-uses-react': RULES.OFF,
+    'react/react-in-jsx-scope': RULES.OFF,
+    'react/display-name': RULES.WARN,
+    'react/no-array-index-key': RULES.OFF,
+    'react/require-default-props': RULES.OFF,
+    'react/forbid-prop-types': RULES.OFF,
     'jsx-a11y/alt-text': [RULES.WARN, { elements: ['img'], img: ['Image'] }],
     'jsx-a11y/aria-props': RULES.WARN,
     'jsx-a11y/aria-proptypes': RULES.WARN,
     'jsx-a11y/aria-unsupported-elements': RULES.WARN,
     'jsx-a11y/role-has-required-aria-props': RULES.WARN,
     'jsx-a11y/role-supports-aria-props': RULES.WARN,
-    "prettier/prettier": [RULES.ERROR, {
-      "singleQuote": true,
-      "semi": false,
-      "trailingComma": "es5"
-    }],
+    'prettier/prettier': [
+      RULES.ERROR,
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'es5',
+      },
+    ],
     'jsdoc/require-jsdoc': [
       RULES.ERROR,
       {
@@ -113,21 +140,21 @@ module.exports = {
       RULES.ERROR,
       'block',
       [
-        " ------------------------------------------------------------------------- *",
-        " * Copyright 2002-2021, OpenNebula Project, OpenNebula Systems               *",
-        " *                                                                           *",
-        " * Licensed under the Apache License, Version 2.0 (the \"License\"); you may   *",
-        " * not use this file except in compliance with the License. You may obtain   *",
-        " * a copy of the License at                                                  *",
-        " *                                                                           *",
-        " * http://www.apache.org/licenses/LICENSE-2.0                                *",
-        " *                                                                           *",
-        " * Unless required by applicable law or agreed to in writing, software       *",
-        " * distributed under the License is distributed on an \"AS IS\" BASIS,         *",
-        " * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *",
-        " * See the License for the specific language governing permissions and       *",
-        " * limitations under the License.                                            *",
-        " * ------------------------------------------------------------------------- "
+        ' ------------------------------------------------------------------------- *',
+        ' * Copyright 2002-2021, OpenNebula Project, OpenNebula Systems               *',
+        ' *                                                                           *',
+        ' * Licensed under the Apache License, Version 2.0 (the "License"); you may   *',
+        ' * not use this file except in compliance with the License. You may obtain   *',
+        ' * a copy of the License at                                                  *',
+        ' *                                                                           *',
+        ' * http://www.apache.org/licenses/LICENSE-2.0                                *',
+        ' *                                                                           *',
+        ' * Unless required by applicable law or agreed to in writing, software       *',
+        ' * distributed under the License is distributed on an "AS IS" BASIS,         *',
+        ' * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *',
+        ' * See the License for the specific language governing permissions and       *',
+        ' * limitations under the License.                                            *',
+        ' * ------------------------------------------------------------------------- ',
       ],
     ],
   },
